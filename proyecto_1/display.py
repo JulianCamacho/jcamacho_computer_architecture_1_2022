ec9350fac299e42f1ad99042d71a51f1db0a48d1
JulianCamacho <jjulian.341@gmail.com>
2023-03-03 23:11:31 -0600

    Se agrega funcionalidad para cargar la imagen encriptada desde el archivo

--- proyecto_1/display.py
+++ proyecto_1/display.py
@@ -3,18 +3,28 @@ from PIL import Image, ImageTk
 
 # Ventana de Tkinter
 window = tk.Tk()
-window.geometry("640x960")
 
 # Cargar imagenes
-encrypted_img = Image.open("test.png")
+# Manejo del archivo 
+with open('5.txt', 'r') as f:
+    data = f.read().split()
+
+# Convertir pixeles a enteros 
+data = [int(x) for x in data]
+encrypted_img = Image.new('L', (640, 960))
+encrypted_img.putdata(data)
 decrypted_img = Image.open("test.png")
+
+# Crear imagenes
 encrypted_photo = ImageTk.PhotoImage(encrypted_img)
 decrypted_photo = ImageTk.PhotoImage(decrypted_img)
 
-# Crear labels
+# Crear labels para desplegar las imagenes lado a lado 
 encrypted_label = tk.Label(window, image=encrypted_photo)
 encrypted_label.pack(side="left")
 decrypted_label = tk.Label(window, image=decrypted_photo)
 decrypted_label.pack(side="right")
 
 window.mainloop()
+
+
