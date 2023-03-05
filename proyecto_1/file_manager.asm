3fd46eae82f447a454b21b00397c1b2d948f38b5
JulianCamacho <jjulian.341@gmail.com>
2023-03-04 20:30:14 -0600

    Se agrega función para crear un archivo desde ensamblador

--- proyecto_1/file_manager.asm
+++ proyecto_1/file_manager.asm
@@ -2,36 +2,43 @@
 
 ; Entradas:
 ;       edi: nombre del archivo
-;       mode: definir modo de acceso
 
-open_file:
+open_input_file:
     push ebp            ;--> Prologo de la funcion
     mov ebp, esp        ;-|
 
-    mov eax, 5          ; Codigo de llamada al sistema open() para abrir el archivo
-    mov ebx, edi        ; edi: nombre del archivo
-    mov ecx, mode       ; Definir modo de acceso
-    int 0x80            ; Ejecutar llamada al sistema
-    mov edi, eax        ; Guardar el file descriptor en memoria
+    mov eax, 5                      ; Codigo de llamada al sistema open() para abrir el archivo
+    mov ebx, input_filename         ; edi: nombre del archivo
+    mov ecx, 0                      ; Modo solo lectura
+    int 0x80                        ; Ejecutar llamada al sistema
+    mov [input_fd], eax             ; Guardar el file descriptor en memoria
 
-    mov esp, ebp        ;--> Epilogo de la funcion
-    pop ebp             ;-|
+    mov esp, ebp                    ;--> Epilogo de la funcion
+    pop ebp                         ;-|
     ret
+
+open_ouput_file:
+    mov eax, 5                      ; Codigo de llamada al sistema open() para abrir el archivo
+    mov ebx, output_filename        ; edi: nombre del archivo
+    mov ecx, 0x42                   ; O_CREAT | O_RDWR | O_TRUNC
+    mov edx, 0x1C0                  ; Modo para usuarios    
+    int 0x80                        ; Ejecutar llamada al sistema
+    mov [output_fd], eax            ; Guardar el file descriptor en memoria
+
 
 
 ; Entradas:
-;       edi: nombre del archivo
 ;       buffer: almacena lo que se lee
 
 read_file:
     push ebp
     mov ebp, esp
 
-    mov eax, 3          ; Codigo de llamada al sistema read() para leer el archivo
-    mov ebx, edi        ; File descriptor
-    mov ecx, buffer     ; Buffer para almacenar lo que se lee
-    mov edx, 16         ; Cantidad de bytes por leer
-    int 0x80            ; Ejecutar llamada al sistema
+    mov eax, 3                      ; Codigo de llamada al sistema read() para leer el archivo
+    mov ebx, [input_fd]             ; File descriptor
+    mov ecx, buffer                 ; Buffer para almacenar lo que se lee
+    mov edx, 18                     ; Cantidad de bytes por leer
+    int 0x80                        ; Ejecutar llamada al sistema
 
     mov esp, ebp
     pop ebp
@@ -40,44 +47,45 @@ read_file:
 
 
 ; Entradas:
-;       edi: nombre del archivo
+;       edx: valor por escribir
 
 write_file:
-    mov eax, 4          ; Codigo de llamada al sistema write() para escribir en el archivo
-    mov ebx, edi        ; File descriptor
-    mov ecx, edx        ; 
-    mov edx, 1          ; Cantidad de bytes por escribir
-    int 0x80            ; call kernel
+    mov eax, 4                  ; Codigo de llamada al sistema write() para escribir en el archivo
+    mov ebx, [output_fd]        ; File descriptor
+    mov ecx, pixel              ; Valor por escribir
+    mov edx, 1                  ; Cantidad de bytes por escribir
+    int 0x80                    ; Ejecutar llamada al sistema
     ret
 
 
-
-; Entradas:
-;       edi: nombre del archivo
-
-close_file:
+close_input_file:
     push ebp
     mov ebp, esp
 
-    mov eax, 6          ; Codigo de llamada al sistema read() para leer el archivo
-    mov ebx, edi        ; File descriptor
-    int 0x80            ; Ejecutar llamada al sistema
+    mov eax, 6                  ; Codigo de llamada al sistema read() para leer el archivo
+    mov ebx, [input_fd]         ; File descriptor
+    int 0x80                    ; Ejecutar llamada al sistema
 
     mov esp, ebp
     pop ebp
     ret
 
 
+close_output_file:
+    mov eax, 6                  ; Codigo de llamada al sistema read() para leer el archivo
+    mov ebx, [output_fd]        ; File descriptor
+    int 0x80                    ; Ejecutar llamada al sistema
+    ret
 
 print_buffer:
     push ebp
     mov ebp, esp
 
-    mov eax, 4          ; Codigo de llamada al sistema write()
-    mov ebx, 1          ; File descriptor para stdout
-    mov ecx, buffer     ; Buffer que contiene lo que se va a imprimir
-    mov edx, 16         ; Cantidad de bytes por imprimir
-    int 0x80            ; Ejecutar llamada al sistema
+    mov eax, 4                  ; Codigo de llamada al sistema write()
+    mov ebx, 1                  ; File descriptor para stdout
+    mov ecx, buffer             ; Buffer que contiene lo que se va a imprimir
+    mov edx, 18                 ; Cantidad de bytes por imprimir
+    int 0x80                    ; Ejecutar llamada al sistema
 
     mov esp, ebp
     pop ebp
--- proyecto_1/test.asm
+++ proyecto_1/test.asm
@@ -2,33 +2,28 @@
 %include "file_manager.asm" 
 
 section .data
-    output_filename db "7.txt", 0
-    input_filename db "6.txt", 0
-    mode     db 0x00           ; Modo de solo lectura
-    buffer   times 16 db 0     ; Buffer para leer 16 bits a la vez
-    input_fd dd 0              ; File descriptor para el archivo de entrada
-    output_fd dd 0              ; File descriptor para el archivo de salida
- 
+    output_filename     db "7.txt", 0
+    input_filename      db "6.txt", 0
+    buffer   times 18   db 0              ; Buffer para leer 16 bits a la vez
+    input_fd            dd 0              ; File descriptor para el archivo de entrada
+    output_fd           dd 0              ; File descriptor para el archivo de salida
+    pixel               dw 0x12           ; Pixel desencriptado por guardar
+
 section .text
     global _start
 
 _start:
-    mov edi, input_filename
-    call open_file
-    mov dword [input_fd], eax  ; Guardar el fd del valor de retorno
-
-read_loop:
+    call open_input_file
     call read_file
-
     cmp eax, 0           ; Verificar si es el final del archivo
-    ;jle done            ; Si lo es, ir a cerrarlo
-
     call print_buffer
-    jle done
-    ;jmp read_loop
+    call close_input_file
 
-done:
-    call close_file
+    call open_ouput_file
+    mov  ebx, [buffer]
+    mov  [pixel], ebx 
+    call write_file
+    call close_output_file
 
     ; Salir
     mov eax, 1
