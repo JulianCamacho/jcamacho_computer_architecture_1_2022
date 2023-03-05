eb75fdcf279c10f99858b95869910026fd1e6436
JulianCamacho <jjulian.341@gmail.com>
2023-03-04 18:55:45 -0600

    Se agrega parametrización por registros a funciones de manejo de archivos

--- proyecto_1/file_manager.asm
+++ proyecto_1/file_manager.asm
@@ -1,25 +1,34 @@
 ;;======== FUNCIONES DE MANEJO DE ARCHIVOS ========
 
+; Entradas:
+;       edi: nombre del archivo
+;       mode: definir modo de acceso
+
 open_file:
     push ebp            ;--> Prologo de la funcion
     mov ebp, esp        ;-|
 
     mov eax, 5          ; Codigo de llamada al sistema open() para abrir el archivo
-    mov ebx, filename   ; Nombre del archivo
-    mov ecx, mode       ; Modo solo lectura
+    mov ebx, edi        ; edi: nombre del archivo
+    mov ecx, mode       ; Definir modo de acceso
     int 0x80            ; Ejecutar llamada al sistema
-    mov [fd], eax       ; Guardar el file descriptor en memoria
+    mov edi, eax        ; Guardar el file descriptor en memoria
 
     mov esp, ebp        ;--> Epilogo de la funcion
     pop ebp             ;-|
     ret
+
+
+; Entradas:
+;       edi: nombre del archivo
+;       buffer: almacena lo que se lee
 
 read_file:
     push ebp
     mov ebp, esp
 
     mov eax, 3          ; Codigo de llamada al sistema read() para leer el archivo
-    mov ebx, [fd]       ; File descriptor
+    mov ebx, edi        ; File descriptor
     mov ecx, buffer     ; Buffer para almacenar lo que se lee
     mov edx, 16         ; Cantidad de bytes por leer
     int 0x80            ; Ejecutar llamada al sistema
@@ -29,12 +38,29 @@ read_file:
     ret
 
 
+
+; Entradas:
+;       edi: nombre del archivo
+
+write_file:
+    mov eax, 4          ; Codigo de llamada al sistema write() para escribir en el archivo
+    mov ebx, edi        ; File descriptor
+    mov ecx, edx        ; 
+    mov edx, 1          ; Cantidad de bytes por escribir
+    int 0x80            ; call kernel
+    ret
+
+
+
+; Entradas:
+;       edi: nombre del archivo
+
 close_file:
     push ebp
     mov ebp, esp
 
     mov eax, 6          ; Codigo de llamada al sistema read() para leer el archivo
-    mov ebx, [fd]       ; File descriptor
+    mov ebx, edi        ; File descriptor
     int 0x80            ; Ejecutar llamada al sistema
 
     mov esp, ebp
--- proyecto_1/test.asm
+++ proyecto_1/test.asm
@@ -2,16 +2,20 @@
 %include "file_manager.asm" 
 
 section .data
-    filename db "6.txt", 0
+    output_filename db "7.txt", 0
+    input_filename db "6.txt", 0
     mode     db 0x00           ; Modo de solo lectura
     buffer   times 16 db 0     ; Buffer para leer 16 bits a la vez
-    fd       dd 0              ; File descriptor
-
+    input_fd dd 0              ; File descriptor para el archivo de entrada
+    output_fd dd 0              ; File descriptor para el archivo de salida
+ 
 section .text
     global _start
 
 _start:
+    mov edi, input_filename
     call open_file
+    mov dword [input_fd], eax  ; Guardar el fd del valor de retorno
 
 read_loop:
     call read_file
