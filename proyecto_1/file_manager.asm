5e18d2cdd52e6882eef8802eacbde3f3559b85a9
JulianCamacho <jjulian.341@gmail.com>
2023-03-11 17:10:48 -0600

    Avance en funcion para convertir ascii a integer

--- proyecto_1/file_manager.asm
+++ proyecto_1/file_manager.asm
@@ -35,12 +35,60 @@ read_file:
     mov eax, 3                      ; Codigo de llamada al sistema read() para leer el archivo
     mov ebx, [input_fd]             ; File descriptor
     mov ecx, buffer                 ; Buffer para almacenar lo que se lee
-    mov edx, 18                     ; Cantidad de bytes por leer
+    mov edx, buf_size               ; Cantidad de bytes por leer
     int 0x80                        ; Ejecutar llamada al sistema
 
     mov esp, ebp
     pop ebp
     ret
+
+
+read_next_number:
+    mov esi, buffer                 ; Esi apunta al buffer
+    mov edi, esi                    ; Edi también
+    next_number:
+        mov ah, [esi]               ; Guardar en ah lo que se encuentra en el buffer
+        cmp ah, 0                   ; Verificar si es el final del buffer
+        je read_done                     ; Terminar lectura
+        cmp ah, ' '                 ; Verificar si hay un espacio
+        jne next_byte               ; Si no lo hay leer siguiente caracter
+        mov byte [esi], 0           ; Colocar null al final (necesario para atoi)
+        mov eax, format             ; Formato del string para atoi
+        mov ecx, edi                ; Colocarse en el inicio del buffer
+
+        jmp my_atoi                 ; Convertir a entero
+
+        inc esi
+        mov edi, esi                ; Empezar siguiente numero
+
+        jmp next_number
+        
+    next_byte:
+        inc esi
+        jmp next_number
+
+    my_atoi:
+        mov eax, ecx                ; Cargar string en eax
+        xor ebx, ebx                
+        mov edx, 10                 ; Base de la conversion
+        convert_loop:
+            cmp byte [eax], 0       ; Verificar final del string
+            je done
+            movzx edi, byte [eax]   ; Cargar un digito
+            sub edi, '0'            ; Conversion ASCII - decimal
+            mul edx                 ; Multiplicar por la base
+            add ebx, edi            ; Sumar en el acumulador
+            inc eax                 ; Moverse a siguiente caracter
+            loop convert_loop       
+        done:
+            mov [pixel], ebx
+            call print_pixel
+
+    read_done:
+        xor ebx, ebx
+
+
+
 
 
 
@@ -82,7 +130,7 @@ print_buffer:
     mov eax, 4                  ; Codigo de llamada al sistema write()
     mov ebx, 1                  ; File descriptor para stdout
     mov ecx, buffer             ; Buffer que contiene lo que se va a imprimir
-    mov edx, 18                 ; Cantidad de bytes por imprimir
+    mov edx, 8                 ; Cantidad de bytes por imprimir
     int 0x80                    ; Ejecutar llamada al sistema
 
     mov esp, ebp
@@ -96,7 +144,7 @@ print_pixel:
     mov eax, 4                  ; Codigo de llamada al sistema write()
     mov ebx, 1                  ; File descriptor para stdout
     mov ecx, pixel              ; Buffer que contiene lo que se va a imprimir
-    mov edx, 18                 ; Cantidad de bytes por imprimir
+    mov edx, 8                 ; Cantidad de bytes por imprimir
     int 0x80                    ; Ejecutar llamada al sistema
 
     mov esp, ebp
--- proyecto_1/test.asm
+++ proyecto_1/test.asm
@@ -3,11 +3,17 @@
 
 section .data
     output_filename     db "7.txt", 0
-    input_filename      db "6.txt", 0
-    buffer   times 18   db 0, 0              ; Buffer para leer 16 bits a la vez
-    pixel    times 18   db 0, 0              ; Pixel desencriptado por guardar
+    input_filename      db "5.txt", 0
+    ;buffer   times 8   db 0, 0              ; Buffer para leer 16 bits a la vez
+    pixel               dw 0, 0              ; Pixel desencriptado por guardar
     input_fd            dd 0, 0              ; File descriptor para el archivo de entrada
     output_fd           dd 0, 0              ; File descriptor para el archivo de salida
+    msb                 db 0, 0
+    lsb                 db 0, 0   
+    buf_size equ 1024
+    buffer db buf_size dup(0)
+    format db "%d", 0
+
 
 section .text
     global _start
@@ -18,24 +24,39 @@ _start:
 
     call read_file
     cmp eax, 0                  ; Verificar si es el final del archivo
+
+    call read_next_number
+
     call print_buffer
 
-    mov esi, buffer             
-    mov edi, pixel            
-    mov ecx, 18                 ; Definir cantidad de ciclos
-    cld                         ; Limpiar la direction flag
-    rep movsb                   ; Copiar n veces esi en edi
 
-    call print_pixel
-    ;Escribir en el archivo y cerrar
-    call open_ouput_file
-    call write_file
 
-    call close_output_file
+    ;mov esi, buffer             
+    ;mov edi, pixel            
+    ;mov ecx, 18                 ; Definir cantidad de ciclos
+    ;cld                         ; Limpiar la direction flag
+    ;rep movsb                   ; Copiar n veces esi en edi
+        
+    ;call print_pixel
+    ;;Escribir en el archivo y cerrar
+    ;call open_ouput_file
+    ;call write_file
+;
+    ;call close_output_file
     call close_input_file
 
 
+    ;mov eax, buffer     ; mueve la dirección del búfer en eax
+    ;sub eax, '0'
+    ;add eax, 5
+
+    ;mov eax, buffer
+    ;call atoi          ; Utilizar la función "atoi" para convertir el número
+
+    ; Almacenar el valor numérico en el registro EAX
+    ;mov eax, ebx
+
     ; Salir
-    mov eax, 1
-    xor ebx, ebx        
+    mov ebx, eax      
+    mov eax, 1 
     int 0x80            
